3a03bc9cf6bbfb0920c32148d09c58590ba13d01
Ettore Forigo <etty.2000.ef@gmail.com>
2017-04-28 02:35:09 +0200

    Upload

---
+++ app/src/main/java/hexwell/thingshvac/MyFirebaseMessagingService.java
@@ -0,0 +1,25 @@
+package hexwell.thingshvac;
+
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+	private LocalBroadcastManager broadcaster;
+
+	@Override
+	public void onCreate() {
+		broadcaster = LocalBroadcastManager.getInstance(this);
+	}
+
+	@Override
+	public void onMessageReceived(RemoteMessage remoteMessage) {
+		if (remoteMessage.getData().size() > 0) {
+			Intent intent = new Intent("ServerUpdatedTemp");
+			intent.putExtra("wanted", remoteMessage.getData().get("wanted"));
+			broadcaster.sendBroadcast(intent);
+		}
+	}
+}
